a28c01481dc23f84828013cb9aa42646162e301d
Mark Anjelo Perez <111416671+jelopogi123456@users.noreply.github.com>
2023-01-19 20:41:41 +0800

    Update chatbot.py

--- Jeloy/chatbot.py
+++ Jeloy/chatbot.py
@@ -3,10 +3,8 @@ import json
 import pickle
 import numpy as np
 
-
 import nltk
 from nltk.stem import WordNetLemmatizer
-
 from tensorflow.keras.models import load_model
 
 lemmatizer = WordNetLemmatizer()
@@ -20,28 +18,45 @@ model = load_model('chatbotmodel.h5')
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
-    return sentence_words
+    return " ".join(sentence_words)
 
-
-def bag_of_words(sentence):
-    sentence_words = clean_up_sentence((sentence))
-    bag = [0] * len(words)
-    for w in sentence_words:
-        for i, word in enumerate(words):
-            if word == w:
+def bag_of_words(sentence, words):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    bag = [0]*len(words)
+    for s in sentence_words:
+        for i,word in enumerate(words):
+            if word == s:
                 bag[i] = 1
-    return np.array(bag)
+    return(np.array(bag))
 
 def predict_class(sentence):
-    bow = bag_of_words(sentence)
-    res = model.predict(np.array([bow]))[0]
+    p = bag_of_words(sentence, words)
+    res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.25
-    results = [[i, r] for i ,r in enumerate(res) if r > ERROR_THRESHOLD]
-
-    results.sort(key=lambda x: x[1] , reverse=True)
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
-        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
     return return_list
 
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
+    return result
+
+def chatbot_response(text):
+    ints = predict_class(text)
+    res = getResponse(ints, intents)
+    return res
+
+if model:
+    print("Model Loaded Successfully")
+else:
+    print("Error Loading Model")
 
