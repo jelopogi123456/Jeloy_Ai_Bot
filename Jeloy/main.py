600b5c8c19ee08fa77ddf8d97f824c4dd734132c
Mark Anjelo Perez <111416671+jelopogi123456@users.noreply.github.com>
2023-01-19 18:29:21 +0800

    Add files via upload

---
+++ Jeloy/main.py
@@ -0,0 +1,77 @@
+import random
+import json
+import pickle
+import numpy as np
+
+import nltk
+
+
+from nltk.stem import WordNetLemmatizer
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import SGD
+
+lemmatizer = WordNetLemmatizer()
+with open('intents.json') as json_file:
+    intents = json.load(json_file)
+
+words = []
+classes = []
+documents = []         #documents which are going to be the combinations, the belongings
+ignore_letters = ['?', '!', '.', ',']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        word_list = nltk.word_tokenize(pattern)
+        words.extend(word_list)
+        documents.append((word_list, intent['tag']))     #intent['tag']  is a class in json so you can know that word_list belongs to 'tag'
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+words = sorted(set(words))
+
+classes = sorted(set(classes))
+
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+
+training = []
+output_empty = [0] * len(classes)
+
+
+max_length = max([len(doc) for doc, _ in documents])
+training = []
+for doc, tag in documents:
+    bag = [1 if word in doc else 0 for word in words]
+    # Pad the bag of words representation with zeroes
+    bag += [0] * (max_length - len(bag))
+    output_row = [0] * len(classes)
+    output_row[classes.index(tag)] = 1
+    training.append([bag, output_row])
+
+random.shuffle(training)
+train_x = np.array([train[0] for train in training])
+train_y = np.array([train[1] for train in training])
+
+
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+    initial_learning_rate=0.01,
+    decay_steps=100,
+    decay_rate=0.96
+)
+sgd = SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size = 5 , verbose = 1)
+model.save('chatbotmodel.h5', hist)
+print('Done')
